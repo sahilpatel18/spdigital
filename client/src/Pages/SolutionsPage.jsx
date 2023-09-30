0490e4b62a0363d2e58c6e5cbbd1f10e26b8d193
sahilpatel18 <sahilwebdev@gmail.com>
2023-09-30 16:10:18 -0400

    Initialize Pages

---
+++ client/src/Pages/SolutionsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SolutionsPage = () => {
+  return (
+    <div>SolutionsPage</div>
+  )
+}
+
+export default SolutionsPage
---
+++ client/src/Pages/TechnologyPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TechnologyPage = () => {
+  return (
+    <div>TechnologyPage</div>
+  )
+}
+
+export default TechnologyPage
