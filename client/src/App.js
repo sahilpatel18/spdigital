86f0229a0939c18e9d31f443e5ff8516bace3dfd
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-02 21:34:04 -0400

    feat: Implement app routing with React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,65 @@
-function App() {
-  return <div>App</div>;
-}
+import RegisterPage from "./Pages/RegisterPage";
+import LoginPage from "./Pages/LoginPage";
+import AboutPage from "./Pages/AboutPage";
+import HeroPage from "./Pages/HeroPage";
+import ContactPage from "./Pages/ContactPage";
+import SolutionsPage from "./Pages/SolutionsPage";
+import TechnologyPage from "./Pages/TechnologyPage";
+import Navbar from "./Components/Navbar";
+import WorkPage from "./Pages/WorkPage";
+import CareersPage from "./Pages/CareersPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      element: <Navbar />,
+      children: [
+        {
+          path: "/",
+          element: <HeroPage />,
+        },
+        {
+          path: "/register",
+          element: <RegisterPage />,
+        },
+        {
+          path: "/login",
+          element: <LoginPage />,
+        },
+        {
+          path: "/about",
+          element: <AboutPage />,
+        },
+        {
+          path: "/solutions",
+          element: <SolutionsPage />,
+        },
+        {
+          path: "/technology",
+          element: <TechnologyPage />,
+        },
+        {
+          path: "/contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "/work",
+          element: <WorkPage />,
+        },
+        {
+          path: "/careers",
+          element: <CareersPage />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
 export default App;
