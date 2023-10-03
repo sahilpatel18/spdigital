da81a71b51b193a0645b6337ff5a8d86da1b5abf
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-02 21:35:20 -0400

    feat: Enable CORS for cross-origin requests

--- server/index.js
+++ server/index.js
@@ -4,16 +4,16 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./Routes/routes");
+const cors = require("cors");
 const PORT = process.env.PORT || 5001;
 const MONGO_URI = process.env.MONGO_URI;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", routes);
 
 mongoose.connect(MONGO_URI);
 const db = mongoose.connection;
-
-
 
 db.on("error", (err) => {
   console.log(err);
