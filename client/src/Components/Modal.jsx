829f27855f7da651f251f0f876fa80aab354296a
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-11 16:57:17 -0400

    feat: Create modal for updating user account info

---
+++ client/src/Components/Modal.jsx
@@ -0,0 +1,162 @@
+import { PencilIcon } from "@heroicons/react/24/outline";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const Modal = ({ isOpen, toggleModal, user }) => {
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Required"),
+    company: Yup.string().required("Required"),
+  });
+
+  const handleUpdate = async (userData) => {
+    const config = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+    const response = await fetch(
+      `${process.env.REACT_APP_BASE_URL}/user/${user._id}`,
+      config
+    );
+    if (response.ok) {
+      toggleModal();
+    }
+  };
+
+  if (!isOpen) return null;
+  return (
+    <div className={isOpen ? "fixed inset-0 z-50" : "hidden"}>
+      <div
+        className="absolute inset-0 bg-black opacity-50"
+        onClick={toggleModal}
+      ></div>
+
+      <div
+        id="authentication-modal"
+        tabIndex="-1"
+        aria-hidden="true"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-md p-4 bg-white rounded-lg shadow dark:bg-gray-700"
+      >
+        <div className="relative w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button
+              type="button"
+              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-hide="authentication-modal"
+              onClick={toggleModal}
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="px-6 py-6 lg:px-8">
+              <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                Account Information
+              </h3>
+              <Formik
+                validationSchema={validationSchema}
+                initialValues={{
+                  email: user.email,
+                  company: user.company,
+                }}
+                onSubmit={(values) => handleUpdate(values)}
+              >
+                {({ handleChange }) => (
+                  <Form className="space-y-6">
+                    <div>
+                      <label
+                        htmlFor="name"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Name
+                      </label>
+                      <div className="flex items-center">
+                        <Field
+                          type="text"
+                          name="name"
+                          id="name"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                          defaultValue={user.name}
+                          readOnly
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Email
+                      </label>
+                      <div className="flex items-center">
+                        <Field
+                          type="email"
+                          name="email"
+                          id="email"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                          onChange={handleChange}
+                        />
+                        <PencilIcon className="h-5 w-5 ml-2 text-gray-400" />
+                      </div>
+                      <ErrorMessage
+                        name="email"
+                        component="div"
+                        className="text-red-500 text-sm mt-1"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="company"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Company
+                      </label>
+                      <div className="flex items-center">
+                        <Field
+                          type="text"
+                          name="company"
+                          id="company"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                        />
+                        <PencilIcon className="h-5 w-5 ml-2 text-gray-400" />
+                      </div>
+                      <ErrorMessage
+                        name="company"
+                        component="div"
+                        className="text-red-500 text-sm mt-1"
+                      />
+                    </div>
+
+                    <button
+                      type="submit"
+                      className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Update Account
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
