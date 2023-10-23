4f65bb54554c9a8f80cce032f20c49bd4afa401f
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-23 15:52:56 -0400

    refactor: Replace old loader with Loader component

--- client/src/Pages/AccountsPage.jsx
+++ client/src/Pages/AccountsPage.jsx
@@ -2,8 +2,8 @@ import { CpuChipIcon } from "@heroicons/react/20/solid";
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
-
 import Modal from "../Components/Modal";
+import Loader from "../Components/Loader";
 
 const AccountsPage = () => {
   const { user } = useAuth();
@@ -32,144 +32,92 @@ const AccountsPage = () => {
     setIsModalOpen(!isModalOpen);
   };
 
-  //use for Spinning loader Testing
-  // useEffect(() => {
-  //   // Add a delay of 5 seconds before fetching the data
-  //   const timer = setTimeout(async () => {
-  //     if (user && user.id) {
-  //       const response = await fetch(
-  //         `${process.env.REACT_APP_BASE_URL}/user/${user.id}`
-  //       );
-  //       const data = await response.json();
-  //       setAccount(data);
-  //       setLoading(false);
-  //     } else {
-  //       setLoading(false);
-  //     }
-  //   }, 5000); // 5000ms = 5s
-
-  //   return () => clearTimeout(timer); // Cleanup timer when component is unmounted
-  // }, [user?.id]);
-
-  const loader = (
-    <div className="flex items-center justify-center h-screen">
-      <div
-        className="inline-block h-8 w-8 animate-spin rounded-full border-t-4 border-indigo-600 border-r-transparent border-b-4 border-l-4 align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status"
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
-    </div>
-  );
-
   return loading ? (
-    loader
+    <Loader />
   ) : (
-    <div className="mx-12 my-10 sm:mx-10">
-      <div className="px-4 sm:px-0 flex justify-between items-center">
-        <div>
-          <h3 className="text-base font-semibold leading-7 text-gray-900">
-            Account Information
-          </h3>
-          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-            Personal details and application.
-          </p>
-        </div>
-        {isModalOpen ? (
-          <Modal isOpen={isModalOpen} toggleModal={handleEdit} user={account} />
-        ) : (
-          <button
-            type="button"
-            onClick={handleEdit}
-            className="text-indigo-600 hover:text-white border border-indigo-600 hover:bg-indigo-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-indigo-600 dark:text-indigo-600 dark:hover:text-white dark:hover:bg-indigo-800"
-          >
-            Edit
-          </button>
-        )}
-      </div>
-      <div className="mt-6 border-t border-gray-100">
-        <dl className="divide-y divide-gray-100">
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Name
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {account.name}
-            </dd>
+    <div className="bg-white min-h-screen px-6 py-24 lg:px-8">
+      <div className="max-w-screen-lg mx-auto">
+        <header className="flex justify-between items-center mb-8">
+          <div>
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">
+              Account Information
+            </h1>
+            <p className="mt-2 text-gray-500">
+              Personal details and application.
+            </p>
           </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Email
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {account.email}
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Company
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {account.company}
-            </dd>
+          {isModalOpen ? (
+            <Modal
+              isOpen={isModalOpen}
+              toggleModal={handleEdit}
+              user={account}
+            />
+          ) : (
+            <button
+              type="button"
+              onClick={handleEdit}
+              className="bg-indigo-600 hover:bg-indigo-700 text-white px-5 py-2 rounded-md transition duration-300"
+            >
+              Edit
+            </button>
+          )}
+        </header>
+
+        <section className="bg-white shadow rounded-lg p-6">
+          <div className="mb-6">
+            <h2 className="text-xl font-semibold mb-3">Basic Information</h2>
+            <p>
+              <strong>Name:</strong> {account.name}
+            </p>
+            <p className="mt-2">
+              <strong>Email:</strong> {account.email}
+            </p>
+            <p className="mt-2">
+              <strong>Company:</strong> {account.company}
+            </p>
           </div>
 
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Solutions Purchased
-            </dt>
-            <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <ul className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                {account?.solutionsPurchased?.length < 1 ? (
-                  <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                    <div className="flex w-0 flex-1 items-center">
+          <div className="mt-8">
+            <h2 className="text-xl font-semibold mb-5">Solutions Purchased</h2>
+
+            {account?.solutionsPurchased?.length < 1 ? (
+              <div className="flex items-center justify-between bg-gray-100 p-5 rounded-md">
+                <div className="flex items-center">
+                  <CpuChipIcon
+                    className="h-5 w-5 text-gray-400 mr-3"
+                    aria-hidden="true"
+                  />
+                  <span className="text-gray-600">No solutions purchased</span>
+                </div>
+                <Link
+                  to="/solutions"
+                  className="text-indigo-600 hover:text-indigo-700"
+                >
+                  Browse
+                </Link>
+              </div>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {account?.solutionsPurchased?.map((solution, index) => (
+                  <div
+                    className="rounded-md shadow-md p-5 bg-white hover:shadow-lg transition-shadow duration-200"
+                    key={index}
+                  >
+                    <div className="flex items-center">
                       <CpuChipIcon
-                        className="h-5 w-5 flex-shrink-0 text-gray-400"
+                        className="h-5 w-5 text-gray-400 mr-3"
                         aria-hidden="true"
                       />
-                      <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                        <span className="truncate font-medium">
-                          No solutions purchased
-                        </span>
-                      </div>
+                      <span className="text-gray-700 font-medium">
+                        {solution.title}
+                      </span>
                     </div>
-                    <div className="ml-4 flex-shrink-0">
-                      <Link
-                        to="/solutions"
-                        className="font-medium text-indigo-600 hover:text-indigo-500"
-                      >
-                        Browse
-                      </Link>
-                    </div>
-                  </li>
-                ) : (
-                  account?.solutionsPurchased?.map((solution, index) => {
-                    return (
-                      <li
-                        className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6"
-                        key={index}
-                      >
-                        <div className="flex w-0 flex-1 items-center">
-                          <CpuChipIcon
-                            className="h-5 w-5 flex-shrink-0 text-gray-400"
-                            aria-hidden="true"
-                          />
-                          <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                            <span className="truncate font-medium">
-                              {solution.title}
-                            </span>
-                          </div>
-                        </div>
-                      </li>
-                    );
-                  })
-                )}
-              </ul>
-            </dd>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-        </dl>
+        </section>
       </div>
     </div>
   );
