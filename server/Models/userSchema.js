c6894c55efe6751d6ff68b1b06d647fe1277881f
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-28 15:25:20 -0400

    refactor(database): Update solutionsPurchased field type to use Solution schema

--- server/Models/userSchema.js
+++ server/Models/userSchema.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const Solution = require("./solutionSchema");
 
 const userSchema = new Schema({
   name: String,
   email: String,
   password: String,
   company: String,
-  solutionsPurchased: [{ title: String, cost: Number }],
+  solutionsPurchased: [{ type: mongoose.Schema.Types.ObjectId, ref: Solution }],
 });
 
 const User = model("User", userSchema);
