187ed3e9888bdc5501df814b9550d1eb3412e713
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-29 16:23:11 -0400

    chore(reviews): add dummy data for reviews

---
+++ client/src/data/reviews.js
@@ -0,0 +1,34 @@
+const reviews = [
+    {
+      id: 1,
+      name: "Jane Doe",
+      review: "Absolutely amazing! The quality surpassed my expectations.",
+    },
+    {
+      id: 2,
+      name: "John Smith",
+      review: "Great product, fast delivery. I’m very satisfied.",
+    },
+    {
+      id: 3,
+      name: "Emily Johnson",
+      review: "Fantastic service and the product is worth every penny!",
+    },
+    {
+      id: 4,
+      name: "Tomas Doe",
+      review: "Absolutely amazing! The quality surpassed my expectations.",
+    },
+    {
+      id: 5,
+      name: "Jaden Smith",
+      review: "Great product, fast delivery. I’m very satisfied.",
+    },
+    {
+      id: 6,
+      name: "Michael Johnson",
+      review: "Fantastic service and the product is worth every penny!",
+    },
+  ];
+
+  export default reviews
