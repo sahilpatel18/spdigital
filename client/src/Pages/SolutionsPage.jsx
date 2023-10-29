c7fcdac8b89500a003ab6f3348b68978256a342f
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-29 17:36:47 -0400

    refactor(SolutionsPage): map and render solutions using SolutionCard

--- client/src/Pages/SolutionsPage.jsx
+++ client/src/Pages/SolutionsPage.jsx
@@ -28,7 +28,16 @@ const SolutionsPage = () => {
     <div className="bg-white min-h-screen p-8">
       <GradientBackground />
       <h1 className="text-3xl font-bold text-indigo-500 mb-6">Our Solutions</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {solutions.map((solution) => (
+          <SolutionCard
+            id={solution._id}
+            title={solution.title}
+            price={solution.price}
+            key={solution._id}
+          />
+        ))}
+      </div>
     </div>
   );
 };
