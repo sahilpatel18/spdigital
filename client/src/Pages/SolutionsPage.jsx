ed1a58610cf3c4b1a3379119112ef6a33fba47ef
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-29 16:46:29 -0400

    feat(SolutionsPage): add API data fetching for solutions

--- client/src/Pages/SolutionsPage.jsx
+++ client/src/Pages/SolutionsPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import SolutionCard from "../Components/SolutionCard";
+import GradientBackground from "./GradientBackground";
 
 const SolutionsPage = () => {
-  return (
-    <div>SolutionsPage</div>
-  )
-}
+  const [solutions, setSolutions] = useState([]);
 
-export default SolutionsPage
+  useEffect(() => {
+    const fetchSolutions = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_BASE_URL}/solutions`
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setSolutions(data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSolutions();
+  }, []);
+
+  return (
+    <div className="bg-white min-h-screen p-8">
+      <GradientBackground />
+      <h1 className="text-3xl font-bold text-indigo-500 mb-6">Our Solutions</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+    </div>
+  );
+};
+
+export default SolutionsPage;
