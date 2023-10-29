622a5cd87b3aa7a989c20c7962f405f27b05eedc
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-29 16:21:42 -0400

    refactor: move carousel settings to a separate file

---
+++ client/src/config/carouselSettings.js
@@ -0,0 +1,20 @@
+const carouselSettings = {
+    centerMode: true,
+    infinite: true,
+    centerPadding: "0",
+    slidesToShow: 3,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  export default carouselSettings
