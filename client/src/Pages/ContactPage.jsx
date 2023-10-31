a6459f11504240b9a2fd8a0d2ffa3ad53e390a93
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-30 20:33:08 -0400

    Style(ContactPage): Add GradientColors behind form

--- client/src/Pages/ContactPage.jsx
+++ client/src/Pages/ContactPage.jsx
@@ -3,6 +3,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { ToastContainer, toast } from "react-toastify";
 import { contactFormValidationSchema } from "../validation/contactFormValidationSchema";
 import Loader from "../Components/Loader";
+import GradientBackground from "../Styles/GradientBackground";
 import "react-toastify/dist/ReactToastify.css";
 
 const ContactPage = () => {
@@ -45,6 +46,8 @@ const ContactPage = () => {
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <GradientBackground />
+
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -58,19 +61,7 @@ const ContactPage = () => {
         theme="light"
       />
       {loading && <Loader />}
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] pointer-events-none"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-2xl text-center">
+      <div className="mx-auto max-w-2xl  text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Contact sales
         </h2>
@@ -79,126 +70,127 @@ const ContactPage = () => {
         </p>
       </div>
 
-      <Formik
-        initialValues={{
-          name: "",
-          company: "",
-          email: "",
-          phone: "",
-          message: "",
-        }}
-        validationSchema={contactFormValidationSchema}
-        onSubmit={(values, { resetForm }) => {
-          submitContactForm(values, resetForm);
-        }}
-      >
-        {(formik) => (
-          <Form className="mx-auto mt-16 max-w-xl sm:mt-20">
-            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Full Name
-                </label>
-                <div className="mt-2.5">
-                  <Field
-                    id="name"
-                    name="name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-                <ErrorMessage
-                  name="name"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="company"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Company
-                </label>
-                <div className="mt-2.5">
-                  <Field
-                    id="company"
-                    name="company"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-                <ErrorMessage
-                  name="company"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Email
-                </label>
-                <div className="mt-2.5">
-                  <Field
-                    id="email"
-                    name="email"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="phone"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Phone
-                </label>
-                <div className="mt-2.5">
-                  <Field
-                    id="phone"
-                    name="phone"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-                <ErrorMessage
-                  name="phone"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Message
-                </label>
-                <div className="mt-2.5">
-                  <Field
-                    as="textarea"
-                    name="message"
-                    id="message"
-                    rows={4}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
+      <div className="mx-auto mt-8 max-w-2xl bg-white p-6 rounded-lg shadow-2xl transition-transform  hover:-translate-y-1 hover:scale-105 form-container">
+        <Formik
+          initialValues={{
+            name: "",
+            company: "",
+            email: "",
+            phone: "",
+            message: "",
+          }}
+          validationSchema={contactFormValidationSchema}
+          onSubmit={(values, { resetForm }) => {
+            submitContactForm(values, resetForm);
+          }}
+        >
+          {(formik) => (
+            <Form className="mt-8">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Full Name
+                  </label>
+                  <div className="mt-2.5">
+                    <Field
+                      id="name"
+                      name="name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                   <ErrorMessage
-                    name="message"
+                    name="name"
                     component="div"
                     className="text-red-500"
                   />
                 </div>
-              </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="company"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Company
+                  </label>
+                  <div className="mt-2.5">
+                    <Field
+                      id="company"
+                      name="company"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="company"
+                    component="div"
+                    className="text-red-500"
+                  />
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-2.5">
+                    <Field
+                      id="email"
+                      name="email"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-red-500"
+                  />
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="phone"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Phone
+                  </label>
+                  <div className="mt-2.5">
+                    <Field
+                      id="phone"
+                      name="phone"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="phone"
+                    component="div"
+                    className="text-red-500"
+                  />
+                </div>
 
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Message
+                  </label>
+                  <div className="mt-2.5">
+                    <Field
+                      as="textarea"
+                      name="message"
+                      id="message"
+                      rows={4}
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                    <ErrorMessage
+                      name="message"
+                      component="div"
+                      className="text-red-500"
+                    />
+                  </div>
+                </div>
+              </div>
               <div className="mt-10">
                 <button
                   type="submit"
@@ -207,10 +199,10 @@ const ContactPage = () => {
                   Let's talk
                 </button>
               </div>
-            </div>
-          </Form>
-        )}
-      </Formik>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
