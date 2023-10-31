e416f8112e0a60757e5398a0d707da4e972713e4
sahilpatel18 <sahilwebdev@gmail.com>
2023-10-30 20:37:05 -0400

    style(LoginPage): Add gradient background

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 import { loginPageValidationSchema } from "../validation/loginPageValidationSchema";
 import { useAuth } from "../context/AuthContext";
 import jwtDecode from "jwt-decode";
-import GradientBackground from "./GradientBackground";
+import GradientBackground from "../Styles/GradientBackground";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -46,20 +46,20 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="bg-white min-h-screen px-6 py-24 lg:px-8">
+    <div className="isolate bg-white px-6 py-24 lg:px-8">
       <GradientBackground />
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md text-center">
         <img
           className="mx-auto h-10 w-auto"
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
           alt="Your Company"
         />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        <h2 className="mt-6 text-3xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="mx-auto mt-8 max-w-md bg-white p-6 pb-10 rounded-lg shadow-2xl transition-transform hover:-translate-y-1 hover:scale-105 form-container">
         <Formik
           initialValues={{
             email: "",
@@ -75,17 +75,17 @@ const LoginPage = () => {
               <div>
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
                 >
                   Email address
                 </label>
-                <div className="mt-2">
+                <div className="mt-2.5">
                   <Field
                     id="email"
                     name="email"
                     type="email"
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                   <ErrorMessage
                     name="email"
@@ -95,21 +95,19 @@ const LoginPage = () => {
                 </div>
               </div>
               <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Password
-                  </label>
-                </div>
-                <div className="mt-2">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="mt-2.5">
                   <Field
                     id="password"
                     name="password"
                     type="password"
                     autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                   <ErrorMessage
                     name="password"
@@ -118,10 +116,10 @@ const LoginPage = () => {
                   />
                 </div>
               </div>
-              <div>
+              <div className="mt-8">
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="block w-full rounded-md bg-indigo-600 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Sign in
                 </button>
